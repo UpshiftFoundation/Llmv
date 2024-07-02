cf3c9bce99978061466ea41bb0dd5c4f9b23e96e
ashokmohan8 <108913299+ashokmohan8@users.noreply.github.com>
2024-07-02 12:26:37 -0700

    Update TokenIndex.sv
    Worked on tokenindex and tokenizer structure.  Commented the c code I am trying to convert

--- TokenIndex.sv
+++ TokenIndex.sv
@@ -1,3 +1,44 @@
-struct {
-    
-}
+/*
+Trying to convert a pointer from c to systemverilog
+
+//
+    typedef struct {
+    char *str;
+    int id;
+    } TokenIndex;
+
+//
+*/ 
+
+class TokenIndex;
+    string str;
+    int id; 
+
+    // Constructor
+    function new(string s = "", int i = 0);
+        str = s;
+        id = i;
+    endfunction
+endclass
+
+
+/*
+
+typedef struct {
+    char** vocab;
+    float* vocab_scores;
+    TokenIndex *sorted_vocab;
+    int vocab_size;
+    unsigned int max_token_length;
+    unsigned char byte_pieces[512]; // stores all single-byte strings
+} Tokenizer;
+
+*/
+// Tokenizer -> is this the right way to go about this?
+class Tokenizer
+    string vocab[];
+    real vocab_scores[]; // is float the correct type to use?
+    int sorted_vocab[];
+    int max_token_length;
+    byte byte_pieces[512];
+endclass 
